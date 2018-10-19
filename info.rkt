40e51fad055488d08bbcea7c1fd7f39e6bc3b073
srfoster <stephen@thoughtstem.com>
2018-10-19 15:43:20 -0700

    messed up dep

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define version "0.0.1")
 
 (define deps '("aws" "request" "gregor" 
-  "https://github.com/thoughtstem/racket-bricks.git" 
+  "https://github.com/thoughtstem/racket-blocks.git?path=racket-bricks"
   "simple-qr"))
