22097de556a6a9e7b71d6563b01b10b953617738
aBlender <JasonLongLe@gmail.com>
2019-12-23 14:56:04 -0800

    added more product and sku functions and updated example

--- examples/mc-course-to-stripe.rkt
+++ examples/mc-course-to-stripe.rkt
@@ -1,13 +1,41 @@
 #lang racket
 
 (require ts-racket
-         (prefix-in dallas: metacoders-dot-org-dallas-site))
+         (prefix-in dallas: metacoders-dot-org-dallas-site)
+         (prefix-in reno:   metacoders-dot-org-reno-site))
 
-(set-stripe-key! TEST)
+; Set to TEST or LIVE
+; .stripe-test-key and .stripe-key must exist in home directory
+(set-stripe-key! TEST) 
 
-(set-product-name "prod_GKyO5SUplscDn4" "Coding Classes in Space")
+; ==== SET A CUSTOM NAME FOR A PRODUCT ====
+(set-product-name "prod_GKyO5SUplscDn4" "Coding Classes in Dallas")
 
-;(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-winter))
-;(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-winter))
-;(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-spring))
-;(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-spring))
+; ==== CREATE OR UPDATE A CITY PRODUCT ====
+(city->stripe "prod_GKyO5SUplscDn4" "Dallas")
+
+; ==== CREATE OR UPDATE A COURSE SKU ====
+(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-winter))
+(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-winter))
+(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-spring))
+(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-spring))
+
+(city->stripe "prod_reno" "Reno")
+
+(course->stripe "prod_reno" (reno:weekly-class-k-2nd-winter))
+(course->stripe "prod_reno" (reno:weekly-class-3rd-6th-winter))
+(course->stripe "prod_reno" (reno:weekly-class-k-2nd-spring))
+(course->stripe "prod_reno" (reno:weekly-class-3rd-6th-spring))
+
+; ==== SHOW ALL PRODUCTS/CITY ====
+(show-products)
+
+; ==== SHOW ALL SKUS ====
+(show-skus)
+
+; ==== SHOW SKUS LINKED TO A PRODUCT/CITY ====
+(show-skus "prod_reno")
+
+; ==== DELETE PRODUCT AND SKU ====
+#;(delete-product "prod_id")  ;Only works if all link skus are deleted first
+#;(delete-sku "sku_id")
--- lang/stripe-util.rkt
+++ lang/stripe-util.rkt
@@ -3,9 +3,18 @@
 (provide set-stripe-key!
          TEST
          LIVE
+         
          set-product-name
+         city->stripe
          course->stripe
+         
+         stripe-get-status
+         stripe-get-data
+         
+         show-products
+         show-skus
          delete-sku
+         delete-product
          )
 
 (require stripe-integration
@@ -60,10 +69,30 @@
   (let-values ([(status headers json ik) response])
     status))
 
+(define (stripe-get-status url) (get-response-status (stripe-get url)))
+
+(define-syntax-rule (get-response-data response)
+    (let-values ([(status headers json ik) response])
+    json))
+
+(define (stripe-get-data url) (get-response-data (stripe-get url)))
+
+;if it exists, update it, and if not, create it
+(define (city->stripe prod-id city-name)
+  (define name (~a "Coding Classes in " city-name))
+  (if (eq? (stripe-get-status (~a "/v1/products/" prod-id)) 404)
+      (stripe-post (~a "/v1/products")
+                   (hash 'id prod-id
+                         'name name
+                         (string->symbol "attributes[]") "name"
+                         'type "good"))
+      (stripe-post (~a "/v1/products/" prod-id)
+                   (hash 'name name))))
+
 ;if it exists, update it, and if not, create it
 (define (course->stripe prod-id c)
   (define c-sku (course-sku c))
-  (if (eq? (get-response-status (stripe-get (~a "/v1/skus/" c-sku))) 404)
+  (if (eq? (stripe-get-status (~a "/v1/skus/" c-sku)) 404)
       (stripe-post (~a "/v1/skus")
                    (hash (string->symbol "attributes[name]") (course->sku-name c)
                          'id c-sku
@@ -79,8 +108,45 @@
                          (course->sku-name c)
                          'image "https://metacoders.org/img/weekly-classes.jpg"
                          'price (~a (* 100 (course-price c)))
+                         'product prod-id ;this will link the existing sku to a new product
                          )
                    )))
 
+; this only works if all linked skus are deleted first
+(define (delete-product prod-id)
+  (stripe-delete (~a "/v1/products/" prod-id)))
+
 (define (delete-sku sku)
   (stripe-delete (~a "/v1/skus/" sku)))
+
+(define (format-date d)
+    (~a (date-month d) "/" (date-day d) "/" (date-year d)
+      " " (date-hour d) ":" (date-minute d) ":" (date-second d)))
+
+(define (show-products)
+  (displayln (~a (~a "NAME"           #:width 40 #:limit-marker "...") "| "
+                 (~a "PRODUCT ID"     #:width 20 #:limit-marker "...") "| "
+                 (~a "LAST UPDATED " #:width 20 #:limit-marker "...") "\n"
+                 (~a #:width 84 #:pad-string "-") "\n"
+                 (apply ~a (map (λ(p) (~a (~a (hash-ref p 'name)    #:width 40 #:limit-marker "...") "| "
+                                          (~a (hash-ref p 'id)      #:width 20 #:limit-marker "...") "| "
+                                          (~a (format-date (seconds->date (hash-ref p 'updated))) #:width 20 #:limit-marker "...") "\n"))
+                                (hash-ref (stripe-get-data "/v1/products") 'data))))))
+
+(define (show-skus [product-id #f])
+  (displayln (~a (~a "NAME"       #:width 40 #:limit-marker "...") "| "
+                 (~a "PRICE"      #:width 6 #:limit-marker "...") "| "
+                 (~a "SKU ID"     #:width 20 #:limit-marker "...") "| "
+                 (~a "PRODUCT ID"     #:width 20 #:limit-marker "...") "| "
+                 (~a "LAST UPDATED " #:width 20 #:limit-marker "...") "\n"
+                 (~a #:width 114 #:pad-string "-") "\n"
+                 (apply ~a (map (λ(p) (~a (~a (hash-ref (hash-ref p 'attributes) 'name) #:width 40 #:limit-marker "...") "| "
+                                           (~a "$" (/ (hash-ref p 'price) 100)  #:width 6 #:limit-marker "...") "| "
+                                           (~a (hash-ref p 'id)      #:width 20 #:limit-marker "...") "| "
+                                           (~a (hash-ref p 'product)      #:width 20 #:limit-marker "...") "| "
+                                           (~a (format-date (seconds->date (hash-ref p 'updated))) #:width 20 #:limit-marker "...") "\n"))
+                                 (filter (if product-id
+                                             (λ(s) (string=? (hash-ref s 'product) product-id))
+                                             identity)
+                                         (hash-ref (stripe-get-data "/v1/skus") 'data)))))))
+
