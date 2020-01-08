0ca1fd9e9482a23e7fdf151b5a8c5f719b8e74e7
aBlender <JasonLongLe@gmail.com>
2020-01-08 14:21:55 -0800

    Added discount to stripe function

--- examples/mc-course-to-stripe.rkt
+++ examples/mc-course-to-stripe.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require ts-racket
+(require ts-racket/lang/stripe-util
          (prefix-in dallas: metacoders-dot-org-dallas-site)
          (prefix-in reno:   metacoders-dot-org-reno-site))
 
@@ -15,17 +15,17 @@
 (city->stripe "prod_GKyO5SUplscDn4" "Dallas")
 
 ; ==== CREATE OR UPDATE A COURSE SKU ====
-(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-winter))
-(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-winter))
-(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-k-2nd-spring))
-(course->stripe "prod_GKyO5SUplscDn4" (dallas:weekly-class-3rd-6th-spring))
+(course->stripe "prod_GKyO5SUplscDn4" dallas:weekly-class-k-2nd-winter)
+(course->stripe "prod_GKyO5SUplscDn4" dallas:weekly-class-3rd-6th-winter)
+(course->stripe "prod_GKyO5SUplscDn4" dallas:weekly-class-k-2nd-spring)
+(course->stripe "prod_GKyO5SUplscDn4" dallas:weekly-class-3rd-6th-spring)
 
 (city->stripe "prod_reno" "Reno")
 
-(course->stripe "prod_reno" (reno:weekly-class-k-2nd-winter))
-(course->stripe "prod_reno" (reno:weekly-class-3rd-6th-winter))
-(course->stripe "prod_reno" (reno:weekly-class-k-2nd-spring))
-(course->stripe "prod_reno" (reno:weekly-class-3rd-6th-spring))
+(course->stripe "prod_reno" reno:weekly-class-k-2nd-winter)
+(course->stripe "prod_reno" reno:weekly-class-3rd-6th-winter)
+(course->stripe "prod_reno" reno:weekly-class-k-2nd-spring)
+(course->stripe "prod_reno" reno:weekly-class-3rd-6th-spring)
 
 ; ==== SHOW ALL PRODUCTS/CITY ====
 (show-products)
--- lang/stripe-util.rkt
+++ lang/stripe-util.rkt
@@ -56,23 +56,14 @@
 
 (define (course->sku-name c)
   ; === SINGLE-LINE FORMATTING ===
-  (~a (course-location c) " - "
+  (~a (if (> (course-discount c) 0)
+          (~a "Discounted from $" (course-price c) "/student! ")
+          "")
+      (course-location c) " - "
       (course-topic c) " (" (course-grade-range c) ") -  "
       (meeting-date->weekday (first (course-meeting-dates c)))"s from "
       (course-start-time c) "-" (course-end-time c) " - "
-      "Dates: " (apply (curry ~a #:separator ", ") (course-meeting-dates c)))
-  ; === MULTI-LINE FORMATTING ===" this doesn't work consistently without a monospace font
-  #;(apply (curry ~a #:separator " ") (append (map (compose (curry ~a #:width 46 #:pad-string "=" #:align 'left)
-                          (λ(s) (~a s " ")))
-      (list (~a "Topic: " (course-topic c))
-            (~a "Grades: " (course-grade-range c))
-            (~a "Total Meetings: " (length (course-meeting-dates c)))
-            (~a "Meets On: " (meeting-date->weekday (first (course-meeting-dates c)))"s")
-            (~a "Time: " (course-start-time c) " - " (course-end-time c))
-            ))
-      (list (~a "Dates: " (apply (curry ~a #:separator ", ") (course-meeting-dates c))))
-      ))
-  )
+      "Dates: " (apply (curry ~a #:separator ", ") (course-meeting-dates c))))
 
 (define-syntax-rule (get-response-status response)
   (let-values ([(status headers json ik) response])
@@ -106,7 +97,7 @@
                    (hash (string->symbol "attributes[name]") (course->sku-name c)
                          'id c-sku
                          'image "https://metacoders.org/img/weekly-classes.jpg"
-                         'price (~a (* 100 (course-price c)))
+                         'price (~a (* 100 (- (course-price c) (course-discount c))))
                          'currency "usd"
                          (string->symbol "inventory[type]") "infinite"
                          'product prod-id
@@ -115,8 +106,8 @@
       (stripe-post (~a "/v1/skus/" c-sku)
                    (hash (string->symbol "attributes[name]")
                          (course->sku-name c)
-                         'image "https://metacoders.org/img/weekly-classes.jpg"
-                         'price (~a (* 100 (course-price c)))
+                         ;'image "https://metacoders.org/img/weekly-classes.jpg"
+                         'price (~a (* 100 (- (course-price c) (course-discount c))))
                          'product prod-id ;this will link the existing sku to a new product
                          )
                    )))
