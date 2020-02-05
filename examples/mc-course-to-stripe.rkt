894bb1f44603d6457eac6303c3fa72932a796d31
Lindsey Handley <lindseyhandley@gmail.com>
2020-02-05 09:46:10 -0800

    working on discounts

--- examples/mc-course-to-stripe.rkt
+++ examples/mc-course-to-stripe.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require ts-racket
-         (prefix-in reno: metacoders-dot-org-reno-site)
+         (prefix-in dallas: metacoders-dot-org-dallas-site)
          )
 
 ; Set to TEST or LIVE
@@ -15,10 +15,27 @@
 ;(set-product-name "prod_reno" "Coding Classes in reno")
 
 ; ==== CREATE OR UPDATE A COURSE SKU ====
-(course->stripe "prod_reno" reno:weekly-class-k-2nd-winter)
-(course->stripe "prod_reno" reno:weekly-class-3rd-6th-winter)
-(course->stripe "prod_reno" reno:weekly-class-k-2nd-spring)
-(course->stripe "prod_reno" reno:weekly-class-3rd-6th-spring)
+(course->stripe "prod_dallas" dallas:summer-camp-zoo-k-2nd-7/6)
+(course->stripe "prod_dallas" dallas:summer-camp-cartoon-k-2nd-7/6)
+(course->stripe "prod_dallas" dallas:summer-camp-fortnite-3-6th-7/6)
+(course->stripe "prod_dallas" dallas:summer-camp-harry-3-6th-7/6)
+(course->stripe "prod_dallas" dallas:summer-camp-farm-k-2nd-7/13)
+(course->stripe "prod_dallas" dallas:summer-camp-pokemon-k-2nd-7/13)
+(course->stripe "prod_dallas" dallas:summer-camp-minecraft-3rd-6th-7/13)
+(course->stripe "prod_dallas" dallas:summer-camp-marvel-3rd-6th-7/13)
+(course->stripe "prod_dallas" dallas:summer-camp-cartoon-k-2nd-7/20)
+(course->stripe "prod_dallas" dallas:summer-camp-zoo-k-2nd-7/20)
+(course->stripe "prod_dallas" dallas:summer-camp-starwars-3rd-6th-7/20)
+(course->stripe "prod_dallas" dallas:summer-camp-pokemon-3-6th-7/20)
+(course->stripe "prod_dallas" dallas:summer-camp-pokemon-k-2nd-7/27)
+(course->stripe "prod_dallas" dallas:summer-camp-farm-k-2nd-7/27)
+(course->stripe "prod_dallas" dallas:summer-camp-mario-3-6th-7/27)
+(course->stripe "prod_dallas" dallas:summer-camp-fortnite-3-6th-7/27)
+(course->stripe "prod_dallas" dallas:summer-camp-zoo-k-2nd-8/3)
+(course->stripe "prod_dallas" dallas:summer-camp-cartoon-k-2nd-8/3)
+(course->stripe "prod_dallas" dallas:summer-camp-harrypotter-3-6th-8/3)
+(course->stripe "prod_dallas" dallas:summer-camp-minecraft-3-6th-8/3)
+
 
 ; ==== SHOW ALL PRODUCTS/CITY ====
 ;(show-products)
@@ -27,8 +44,8 @@
 ;(show-skus)
 
 ; ==== SHOW SKUS LINKED TO A PRODUCT/CITY ====
-(show-skus "prod_reno")
-(show-skus "prod_temecula")
+;(show-skus "prod_dallas")
+;(show-skus "prod_temecula")
 
 ; ==== DELETE PRODUCT AND SKU ====
 ;(delete-product "prod_GOdy4dsdmBozDq ")  ;Only works if all link skus are deleted first
