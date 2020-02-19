e3a3e7380a34d6050922ee83378020e0336bbd11
jordanT546 <jordan@thoughtstem.com>
2020-02-18 16:27:10 -0800

    fixed stray paren

--- lang/making-courses.rkt
+++ lang/making-courses.rkt
@@ -17,7 +17,6 @@
                                                              "03/18/2020, 03/25/2020, 04/01/2020, 04/08/2020, 04/15/2020, 04/22/2020, 04/29/2020, 05/06/2020, 05/13/2020"))
 
                                   
-(
 
 (make-course-with-meetings-from-bundle! (3-5-spring-2020-bundle "Painted Rock Elementary")
 
