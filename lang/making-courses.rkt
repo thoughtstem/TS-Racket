bc3fbb5640ee85275f55964ff503448452cf550b
jordanT546 <jordan@thoughtstem.com>
2020-02-18 16:12:38 -0800

    updates to code

--- lang/making-courses.rkt
+++ lang/making-courses.rkt
@@ -5,7 +5,7 @@
 
 
 
-#;(make-course-with-meetings-from-bundle! (k2-spring-2020-bundle "Painted Rock Elementary")
+(make-course-with-meetings-from-bundle! (k2-spring-2020-bundle "Painted Rock Elementary")
 
                                         painted-rock
                                         215
@@ -17,9 +17,9 @@
                                                              "03/18/2020, 03/25/2020, 04/01/2020, 04/08/2020, 04/15/2020, 04/22/2020, 04/29/2020, 05/06/2020, 05/13/2020"))
 
                                   
+(
 
-
-#;(make-course-with-meetings-from-bundle! (3-5-spring-2020-bundle "Painted Rock Elementary")
+(make-course-with-meetings-from-bundle! (3-5-spring-2020-bundle "Painted Rock Elementary")
 
                                         painted-rock
                                         215
