58dc4d6a6221743bbc9f9d92b6000dd7b35cbc64
aBlender <JasonLongLe@gmail.com>
2020-02-24 11:31:54 -0800

    Cleaning up lang folder

--- examples/making-courses.rkt
+++ examples/making-courses.rkt
@@ -4,16 +4,44 @@
 (set-env! PROD)
 
 
-(make-course-with-meetings-from-bundle! (3-5-2019-2020-winter-bundle " ")
-                                        holly-drive
-                                        0
-                                        45
+
+(make-course-with-meetings-from-bundle! (k2-spring-2020-bundle "Painted Rock Elementary")
+
+                                        painted-rock
+                                        215
+                                        60
                                         ""
  
 
-                                        (date-strings->dates "12:00pm"
-                                                             "09/09/2019, 09/16/2019, 09/23/2019, 09/30/2019, 10/07/2019, 10/14/2019, 10/21/2019"))
+                                        (date-strings->dates "1:30pm"
+                                                             "03/18/2020, 03/25/2020, 04/01/2020, 04/08/2020, 04/15/2020, 04/22/2020, 04/29/2020, 05/06/2020, 05/13/2020"))
 
-                                        
+                                  
+
+(make-course-with-meetings-from-bundle! (3-5-spring-2020-bundle "Painted Rock Elementary")
+
+                                        painted-rock
+                                        215
+                                        60
+                                        ""
+ 
+
+                                        (date-strings->dates "1:30pm"
+                                                             "03/18/2020, 03/25/2020, 04/01/2020, 04/08/2020, 04/15/2020, 04/22/2020, 04/29/2020, 05/06/2020, 05/13/2020"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- lang/roster code.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require ts-racket)
-
-(set-env! PROD)
-
-;generate roster
-(print-roster 1738)
-
-;generate roster with attendance fillied
